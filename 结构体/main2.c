2ec65f8dae0416e396761fcf9c5a473523b7b2bc
yajie Zhang <2174223295@qq.com>
2021-09-29 16:11:09 +0800

    添加项目文件。

---
+++ 结构体/main2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+struct student
+{
+	int num; char name; char sex;
+	int age; float score; char addr[30];
+};
+
+int main()
+{
+	struct student a = {1940241137,"ÕÅÑÅ½Ü",'M',20,85.4,"°²»Õ" };
+
+}
